fc6d5cbc2e0a89afc9f79991748821f4a2616267
Terror666 <ckuthulu@gmail.com>
2021-08-25 19:10:37 -0500

    licenses

--- index.js
+++ index.js
@@ -1,5 +1,7 @@
 // TODO: Include packages needed for this application
 const inquirer = require('inquirer');
+const fs = require('fs');
+const writeFile = require('./utils/generateMarkdown.js')
 // TODO: Create an array of questions for user input
 const questions = () => {
     return inquirer.prompt([
@@ -30,13 +32,80 @@ const questions = () => {
         {
             type: 'input',
             name: 'installation',
-            message: 'How do you install your project? Provide a step-by-step description'
+            message: 'How do you install your project? Provide a step-by-step description',
+            validate: installationEl => {
+                if (installationEl) {
+                    return true;
+                } else
+                console.log('Please complete installation process');
+                return false;
+            }
         },
+        {
+            type: 'input',
+            name: 'usage',
+            message: 'Provide instructions and examples for use:',
+            validate: usageEl => {
+                if (usageEl) {
+                    return true;
+                } else
+                console.log('Please provide examples/instructions');
+                return false;
+            }
+        },
+        {
+            type: 'confirm',
+            name: 'credits',
+            message: 'Did you have any collaborators? Third-party assets? Tutorials? Additional Creators?',
+            default: true
+        },
+        {
+            type: 'input',
+            name: 'collaborators',
+            message: 'Please list Github username/link/website that you would like to credit',
+            when: ({credits}) => {
+                if (credits) {
+                    return true;
+                } else {
+                    return false;
+                }
+            } 
+        },
+        {
+            type: 'list',
+            name: 'license',
+            message: 'Please choose a license link to attach to your README.md:',
+            choices: ['MIT(short and simple)', 'GNU(General public license v3)', 'Apache v2.0']
+        },
+        {
+            type: 'input',
+            name: 'features',
+            message: '(Optional) If your project has a lot of features, list them here: (if NA, just leave blank)'
+        },
+        {
+            type: 'input',
+            name: 'contributing',
+            message: '(Optional) List any contributing guidlines here (if NA, just leave blank)'
+        },
+        {
+            type: 'input',
+            name: 'tests',
+            message: '(Optional) List any tests and how to run them here (if NA, just leave blank)'
+        },
+        {
+            type: 'confirm',
+            name: 'tableContents',
+            message: 'Do you want your README.md to have a table of contents?',
+            default: false
+        }
     ])
-}
+};
+
 
 // TODO: Create a function to write README file
-function writeToFile(fileName, data) {}
+function writeToFile(fileName, data) {
+ 
+}
 
 // TODO: Create a function to initialize app
 function init() {}
--- utils/generateMarkdown.js
+++ utils/generateMarkdown.js
@@ -1,20 +1,66 @@
 // TODO: Create a function that returns a license badge based on which license is passed in
 // If there is no license, return an empty string
-function renderLicenseBadge(license) {}
+function renderLicenseBadge(license) {
+  if (license === 'MIT(short and simple)') {
+    return '![APM](https://img.shields.io/apm/l/github?color=purple&label=License&logo=MIT&style=plastic)'
+  } else if (license === 'GNU(General public license v3)') {
+    return '![AUR license](https://img.shields.io/aur/license/j?color=red&label=License&logo=GNU)'
+  } else if (license === 'Apache v2.0') {
+    return '![APM](https://img.shields.io/apm/l/github?color=blue&label=Apache%202.0&logo=apache&logoColor=red&style=flat-square)'
+  } else {
+    return '';
+  }
+}
 
 // TODO: Create a function that returns the license link
 // If there is no license, return an empty string
-function renderLicenseLink(license) {}
+function renderLicenseLink(license) {
+  if (license === 'MIT(short and simple)') {
+    return '[MIT License](https://choosealicense.com/licenses/mit/)'
+  } else if (license === 'GNU(General public license v3)') {
+    return '[GNU License](https://choosealicense.com/licenses/gpl-3.0/)'
+  } else if (license === 'Apache v2.0') {
+    return '[Apache v2.0](https://www.apache.org/licenses/LICENSE-2.0)'
+  } else {
+    return '';
+  }
+}
 
 // TODO: Create a function that returns the license section of README
 // If there is no license, return an empty string
 function renderLicenseSection(license) {}
 
+function tcCreate(tableContents) {
+  if (!tableContents) {
+    return `
+
+    * [Installation](#installation)
+    * [Usage](#usage)
+    * [Credits](#credits)
+    * [License](#license)
+     
+    `
+  }
+  return '';
+}
 // TODO: Create a function to generate markdown for README
 function generateMarkdown(data) {
-  return `# ${data.title}
+  return `# ${data.title} 
 
+  ## Description
+  ${data.description}
+  ${tcCreate}
+  ## Installation 
+  ${data.installation}
+
+  ## Usage
+  ${data.usage}
+
+  ## Credits
+  http://${data.collaborators}
+
+  ${renderLicenseSection}
 `;
 }
 
-module.exports = generateMarkdown;
+module.exports = generateMarkdown();
